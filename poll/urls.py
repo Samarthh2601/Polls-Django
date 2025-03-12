889a27247d8ec4b45a3244f74b43d8662de0dd47
Samarthh2601 <samarthdhir57.2014@gmail.com>
2025-03-12 22:31:54 +0530

    Fix add_vote

--- poll/urls.py
+++ poll/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.display_polls, name='display_polls'),
     path('create/', views.create_poll, name='create_poll'),
-    path("my/", views.my_polls, name="my_polls")
+    path('my/', views.my_polls, name='my_polls'),
+    path('add_vote/', views.add_vote, name='vote')
 ]
--- poll/views.py
+++ poll/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse, HttpRequest
-from .models import Poll, Choice
+from .models import Poll, Choice, Vote
 from datetime import datetime
 from django.utils import timezone
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 
+# Displays all the polls from the database
 @login_required
 def display_polls(request: HttpRequest):
     return render(request, 'poll/display_polls.html', context={"polls": Poll.objects.all()})
@@ -42,11 +43,33 @@ def create_poll(request: HttpRequest):
 
     return render(request, "poll/create_poll.html")
 
+
 @login_required
 def my_polls(request: HttpRequest):
+    # Get filtered polls
     polls = Poll.objects.filter(owner=request.user)
     if not polls:
         messages.warning(request, "You have not created any polls yet")
         return redirect("create_poll")
 
     return render(request, 'poll/display_polls.html', context={"polls": polls})
+
+def add_vote(request: HttpRequest):
+    choice_voted = request.POST.get("selected_choice") # Get the selected choice text from the hidden input [poll/display_polls.html]
+    key = request.POST.get("poll_key") #Get the primary key of the selected poll from the hidden input [poll/display_polls.html]
+
+    poll = Poll.objects.get(pk=key) 
+
+    if not poll:
+        messages.error(request, "Error occurred. Please ensure that the poll exists.")
+        return redirect("display_polls")
+        
+    if poll.end_date <= timezone.now():
+        messages.error(request, "Poll has already ended.")
+        return redirect("display_polls")
+
+    choice = Choice.objects.filter(text=choice_voted, poll=poll) # Get selected choice object from db
+    Vote.objects.create(choice=choice, user=request.user)
+
+    messages.success(request, "Successfully voted!")
+    return redirect("display_polls")
